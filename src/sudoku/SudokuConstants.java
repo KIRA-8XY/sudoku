063ae0681d89b107318f2956d99e9167e8d22e02
Beh Siu Li <angelasiuli@gmail.com>
2024-12-04 22:02:34 +0700

    Updating Name and ID Template in Each File!

--- src/sudoku/SudokuConstants.java
+++ src/sudoku/SudokuConstants.java
@@ -4,7 +4,7 @@
  * Group Capstone Project
  * Group #1
  * 1 - 5026221177 - Muhammad Ariq Alwin
- * 2 - Student ID - Student Name 2
+ * 2 - 5026231065 - Beh Siu Li
  * 3 - Student ID - Student Name 3
  */
 
