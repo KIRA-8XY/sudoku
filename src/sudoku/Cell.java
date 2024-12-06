ab602fdc85bea4eea0bad308b048a86d5b10ec6f
KIRA-8XY <ariqalwin94004@gmail.com>
2024-12-06 16:59:29 +0700

    Add Conflict Button and Reset Checker

--- src/sudoku/Cell.java
+++ src/sudoku/Cell.java
@@ -29,53 +29,46 @@ public class Cell extends JTextField {
     public static final Font FONT_NUMBERS = new Font("OCR A Extended", Font.PLAIN, 28);
 
     // Define properties (package-visible)
-    /** The row and column number [0-8] of this cell */
     int row, col;
-    /** The puzzle number [1-9] for this cell */
     int number;
-    /** The status of this cell defined in enum CellStatus */
     CellStatus status;
 
-    /** Constructor */
     public Cell(int row, int col) {
-        super();   // JTextField
+        super();
         this.row = row;
         this.col = col;
-        // Inherited from JTextField: Beautify all the cells once for all
         super.setHorizontalAlignment(JTextField.CENTER);
         super.setFont(FONT_NUMBERS);
     }
 
-    /** Reset this cell for a new game, given the puzzle number and isGiven */
     public void newGame(int number, boolean isGiven) {
         this.number = number;
         status = isGiven ? CellStatus.GIVEN : CellStatus.TO_GUESS;
-        paint();    // paint itself
+        paint();
     }
 
-    /** This Cell (JTextField) paints itself based on its status */
     public void paint() {
         if (status == CellStatus.GIVEN) {
-            // Inherited from JTextField: Set display properties
             super.setText(number + "");
             super.setEditable(false);
             super.setBackground(BG_GIVEN);
             super.setForeground(FG_GIVEN);
         } else if (status == CellStatus.TO_GUESS) {
-            // Inherited from JTextField: Set display properties
             super.setText("");
             super.setEditable(true);
             super.setBackground(BG_TO_GUESS);
             super.setForeground(FG_NOT_GIVEN);
-        } else if (status == CellStatus.CORRECT_GUESS) {  // from TO_GUESS
+        } else if (status == CellStatus.CORRECT_GUESS) {
             super.setBackground(BG_CORRECT_GUESS);
-        } else if (status == CellStatus.WRONG_GUESS) {    // from TO_GUESS
+        } else if (status == CellStatus.WRONG_GUESS) {
             super.setBackground(BG_WRONG_GUESS);
         }
     }
-    public int getNumber(){
+
+    public int getNumber() {
         return number;
     }
+
     public void setStatus(CellStatus status) {
         this.status = status;
     }
@@ -88,4 +81,7 @@ public class Cell extends JTextField {
         return col;
     }
 
+    public boolean isEditable() {
+        return status != CellStatus.GIVEN;
+    }
 }
